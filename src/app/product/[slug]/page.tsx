d8ab75244f5a76a162503108b6a34280abeabbbc
Andrew Porto <andrewluizporto@hotmail.com>
2024-10-07 20:18:24 -0300

    feat: add recommended products to details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,45 +1,47 @@
-import { prismaClient } from "@/lib/prisma"
+import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProdutTotalPrice } from "@/helpers/product";
 import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailsPage {
-    params: {
-        slug: string
-    }
+  params: {
+    slug: string;
+  };
 }
 
-const ProductDetailsPage = async ({params: { slug }}: ProductDetailsPage) => {
-    const product = await prismaClient.product.findFirst({
-        where: {
-            slug: slug,
-        },
+const ProductDetailsPage = async ({ params: { slug } }: ProductDetailsPage) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+    include: {
+      category: {
         include: {
-            category: {
-                include: {
-                    products: {
-                        where: {
-                            slug: {
-                                not: slug,
-                            }
-                        }
-                    },
-                }
-            }
-        }
-    });
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
-    if (!product) return null
-    return(
-        <div className="flex flex-col gap-8 pb-8">
-            <ProductImages imageUrls={product.imageUrls} name={product.name}/>
-            <ProductInfo product={computeProdutTotalPrice(product)}/>
-            <ProductList products={product.category.products}/>
-        </div>
-    )
-}
-     
+  if (!product) return null;
+  return (
+    <div className="flex flex-col gap-8 pb-8">
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo product={computeProdutTotalPrice(product)} />
+      <div>
+        <SectionTitle>Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
+    </div>
+  );
+};
 
- 
 export default ProductDetailsPage;
