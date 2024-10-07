5105633aa95fcd13262d720deb1f27705087d9f2
Andrew Porto <andrewluizporto@hotmail.com>
2024-10-06 22:04:30 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    <span className="font-semibold">2024 By Andrew Porto </span>
+  </div>;
+};
+
+export default Footer;
