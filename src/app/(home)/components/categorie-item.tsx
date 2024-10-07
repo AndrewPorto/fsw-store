30af200d406bfc4090e4376334c317be1cd40f7e
Andrew Porto <andrewluizporto@hotmail.com>
2024-10-06 23:22:42 -0300

    fix: add link on buttons

--- src/app/(home)/components/categorie-item.tsx
+++ src/app/(home)/components/categorie-item.tsx
@@ -1,23 +1,23 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICOM } from "@/constants/category-item";
 import { Category } from "@prisma/client";
-
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-
-
   return (
-    <Badge
-      variant="outline"
-      className="mx-5 flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {CATEGORY_ICOM[category.slug as keyof typeof CATEGORY_ICOM]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="mx-5 flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICOM[category.slug as keyof typeof CATEGORY_ICOM]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -64,9 +64,11 @@ const Header = () => {
           </div>
         </SheetContent>
       </Sheet>
+      <Link href="/">
       <h1 className="text-lg font-semibold">
         <span className="text-primary">FSW</span> Store
       </h1>
+      </Link>
       <Button size="icon" variant={"outline"}>
         <ShoppingCartIcon />
       </Button>
