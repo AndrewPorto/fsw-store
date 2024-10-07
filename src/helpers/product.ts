a8883c38796573919af7bd2cf22619c6ac0dbab7
Andrew Porto <andrewluizporto@hotmail.com>
2024-10-06 22:44:18 -0300

    feat: add CatalogPage

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,6 +1,7 @@
 import { Product } from "@prisma/client";
 
 export interface ProductWithTotalPrice extends Product {
+  imageUrls: any;
   totalPrice: number;
 }
 export const computeProdutTotalPrice = (
