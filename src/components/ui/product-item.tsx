2b5a8a3fdc091c3703e182cc86e9db304b528772
Andrew Porto <andrewluizporto@hotmail.com>
2024-10-07 18:56:59 -0300

    feat: ensure user goes to product details page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -3,56 +3,59 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDown } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          alt={product.name}
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{
-            objectFit: "contain",
-          }}
-        />
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDown size={14}/>
-            {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-      <div>
-        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        <div className="flex items-center gap-2 w-full overflow-hidden text-ellipsis whitespace-nowrap">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className=" font-semibold">
-                R$ {product.totalPrice.toFixed(2)}
-              </p>
-
-              <p className="text-sm line-through opacity-75">
-                R$ {Number(product.basePrice).toFixed(2)}
-              </p>
-            </>
-          ) : (
-            <p className="text-sm font-semibold">
-              R$ {product.basePrice.toFixed(2)}
-            </p>
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex max-w-[156px] flex-col gap-4">
+        <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{
+              objectFit: "contain",
+            }}
+          />
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+              <ArrowDown size={14} />
+              {product.discountPercentage}%
+            </Badge>
           )}
         </div>
+        <div>
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
+
+          <div className="flex w-full items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="font-semibold">
+                  R$ {product.totalPrice.toFixed(2)}
+                </p>
+
+                <p className="text-sm line-through opacity-75">
+                  R$ {Number(product.basePrice).toFixed(2)}
+                </p>
+              </>
+            ) : (
+              <p className="text-sm font-semibold">
+                R$ {product.basePrice.toFixed(2)}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
